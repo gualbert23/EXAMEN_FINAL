cefade4246ca2e571a4fee95ed6d8214e69f6a54
Gualberto Laura <gualbert.laura2@gmail.com>
2022-12-03 03:17:01 -0400

    EXAMEN FINAL - SISTEMA DE PRESTAMOS LARAVEL

---
+++ app/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use \OwenIt\Auditing\Auditable as AuditingAuditable;
+use OwenIt\Auditing\Contracts\Auditable;
+ 
+
+class User extends Authenticatable implements Auditable
+{
+    
+    use Notifiable;
+    use AuditingAuditable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','level'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="card text-center">
+  <div class="card-header">
+    Pie de pagina
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">UNIVERSIDAD PÚBLICA DE EL ALTO</h5>
+    <p class="card-text">Gualberto Laura Mamani.</p>
+    <p class="card-text">7012422LP.</p>
+    
+  </div>
+  <div class="card-footer text-muted">
+    2022
+  </div>
+</div>
